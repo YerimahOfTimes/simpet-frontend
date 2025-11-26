63d95f49ba23bf8dd60fd349051e9c44c3212dc2
YerimahOfTimes <yerimahjr@gmail.com>
2025-11-26 11:51:53 +0100

    Frontend: login fixes, admin panel, axios instance update

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://simpet-backend-1.onrender.com", // Your backend URL
+  baseURL:"https://simpet-backend-1.onrender.com", // Your backend URL
   withCredentials: true, // If you later use cookies/sessions
 });
 
 export default axiosInstance;
+
 
