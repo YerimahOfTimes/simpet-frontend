59e9240c9e0bdbd77708cf05d9bdfb22d0af2d9b
YerimahOfTimes <yerimahjr@gmail.com>
2025-12-01 09:02:24 +0100

    updated commom/index.js

--- src/common/index.js
+++ src/common/index.js
@@ -1,7 +1,7 @@
 // src/common/index.js
 
-// Base URL of backend
-export const API_BASE_URL = "http://localhost:5000/api";
+// Base URL of backend (deployed on Render)
+export const API_BASE_URL = "https://simpet-backend-1.onrender.com/api";
 
 // API endpoints
 export const API = {
